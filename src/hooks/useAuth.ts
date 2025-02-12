d413ead574a8df6b3ffa6e406675347a289cb830
Gyanesh Rao <gyaneshrao28@gmail.com>
2025-02-12 17:33:59 +0530

    de

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -43,7 +43,7 @@ export function useAuth() {
     });
 
     const googleLogin = () => {
-        window.location.href = 'http://localhost:5000/auth/google';
+        window.location.href = 'https://cap-bck-tr.vercel.app/auth/google';
     };
  
     const handleAuthCallback = async (token: string) => {
